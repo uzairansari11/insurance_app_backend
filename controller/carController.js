a6a46f183055ebb0cc39c4fc4b04e29ffa8f0f8d
Uzair Ansari <uzairansari11>
2023-09-17 20:08:41 +0530

    one more change

--- controller/carController.js
+++ controller/carController.js
@@ -1,13 +1,12 @@
-const { CarModel } = require("../model/carModel");
+const { VehicleModel } = require("../model/carModel");
 
 const getCarDetails = async (req, res) => {
-	const carNumber = req.query.car_number; // Get the car_number from query parameters
-	console.log("Car number:", carNumber);
+	const vehicle_number = req.query.car_number; // Get the car_number from query parameters
+	console.log("Car number:", vehicle_number);
 
 	try {
-		const carData = await CarModel.findOne({ car_number: carNumber });
+		const carData = await VehicleModel.findOne(vehicle_number);
 		if (carData) {
-			// Check if carData is truthy (not null)
 			res.status(200).json({ success: true, data: carData });
 		} else {
 			res.status(404).json({ success: false, message: "Car Data Not Found" });
@@ -20,10 +19,11 @@ const getCarDetails = async (req, res) => {
 const postCarDetails = async (req, res) => {
 	try {
 		const carData = req.body;
-		const newCar = new CarModel(carData);
+		const newCar = new VehicleModel(carData);
 		const savedCar = await newCar.save();
 		res.status(201).json({ success: true, data: savedCar });
 	} catch (error) {
+		console.log(error);
 		res.status(500).json({ success: false, error: "Internal server error" });
 	}
 };
--- model/carModel.js
+++ model/carModel.js
@@ -1,24 +1,39 @@
 const mongoose = require("mongoose");
 
-const carSchema = mongoose.Schema({
-	car_number: String,
-	car_owner: String,
-	model_name: String,
-	fuel_type: String,
-	insurance_policy: String,
-	vehicle_color: String,
-	puc_expiry: Date,
-	insurance_date: Date,
-	insurance_expiry_date: Date,
-	challan: [
-		{
-			reason: String,
-			amount: Number,
-			paid: { type: Boolean, default: false },
-		},
-	],
+const policySchema = mongoose.Schema({
+	policy_number: String,
+	start_date: Date,
+	end_date: Date,
+	expiry: { type: Boolean, default: false },
 });
 
-const CarModel = mongoose.model("car", carSchema);
+const challanSchema = mongoose.Schema({
+	reason: String,
+	amount: Number,
+	start_date: Date,
+	paid_date: Date,
+	paid: { type: Boolean, default: false },
+});
 
-module.exports = { CarModel };
+const pucSchema = mongoose.Schema({
+	start_date: Date,
+	paid_date: Date,
+	expiry: { type: Boolean, default: false },
+});
+
+const vehicleSchema = mongoose.Schema({
+	vehicle_number: String,
+	vehicle_owner: String,
+	model_name: String,
+	fuel_type: String,
+	vehicle_type: String,
+	insurance_policies: [policySchema],
+	vehicle_color: String,
+	registration_date: Date,
+	challans: [challanSchema],
+	puc_entries: [pucSchema],
+});
+
+const VehicleModel = mongoose.model("Vehicle", vehicleSchema);
+
+module.exports = { VehicleModel };
