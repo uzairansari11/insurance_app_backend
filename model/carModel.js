574628af52f222055b714c7ea0f0d4551f4d27db
Uzair Ansari <uzairansari11>
2023-09-19 10:44:48 +0530

    chnage3

--- model/carModel.js
+++ model/carModel.js
@@ -4,6 +4,7 @@ const policySchema = mongoose.Schema({
 	policy_number: String,
 	start_date: Date,
 	end_date: Date,
+	policy_name: String,
 	expiry: { type: Boolean, default: false },
 });
 
@@ -27,9 +28,10 @@ const vehicleSchema = mongoose.Schema({
 	model_name: String,
 	fuel_type: String,
 	vehicle_type: String,
-	insurance_policies: [policySchema],
 	vehicle_color: String,
 	registration_date: Date,
+	vehicle_price: Number,
+	insurance_policies: [policySchema],
 	challans: [challanSchema],
 	puc_entries: [pucSchema],
 });
